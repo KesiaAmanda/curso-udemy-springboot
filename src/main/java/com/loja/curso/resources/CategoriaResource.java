6bff412b0a85e4223a63ef3d6b93515eae2480fe
Kesia Amanda <miiizx1@gmail.com>
2021-08-23 13:47:08 -0300

    Adiciona categoria

--- src/main/java/com/loja/curso/resources/CategoriaResource.java
+++ src/main/java/com/loja/curso/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.loja.curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.loja.curso.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
@@ -10,7 +15,15 @@ public class CategoriaResource {
 	
 	@GetMapping
 	//@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Ok";
+	public List<Categoria> listar() {
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		
+		categorias.add(categoria1);
+		categorias.add(categoria2);
+		
+		return categorias;
 	}
 }
