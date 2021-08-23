e363ba2d894d22c02c13278cb65725f459ecb9cf
Kesia Amanda <miiizx1@gmail.com>
2021-08-23 13:57:20 -0300

    Adiciona banco de dados H2

--- src/main/java/com/loja/curso/domain/Categoria.java
+++ src/main/java/com/loja/curso/domain/Categoria.java
@@ -3,10 +3,20 @@ package com.loja.curso.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@Column
 	private String nome;
 	
 	public Categoria() {}
