e5b229473ed2ddd8d54b9d3df9bd072d608d2b1f
Kesia Amanda <kesia.ladeia@tgid.com.br>
2021-09-01 14:39:23 -0300

    Adiciona validação CPF e CNPJ

--- src/main/java/com/loja/curso/dto/ClienteDTO.java
+++ src/main/java/com/loja/curso/dto/ClienteDTO.java
@@ -2,13 +2,24 @@ package com.loja.curso.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.loja.curso.domain.Cliente;
 
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatório!")
+	@Length(min=5, max=80, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Preenchimento obrigatório!")
+	@Email(message = "Email inválido")
 	private String email;
 	
 	public ClienteDTO() {
---
+++ src/main/java/com/loja/curso/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,39 @@
+package com.loja.curso.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.loja.curso.dto.ClienteNewDTO;
+import com.loja.curso.resources.exception.FieldMessage;
+import com.loja.curso.services.validation.utils.BR;
+import com.loja.curso.domain.enums.TipoCliente;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if(objDto.getTipoCliente().equals(TipoCliente.PESSOAFISICA.getCod()) && ! BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		
+		if(objDto.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCod()) && ! BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		// inclua os testes aqui, inserindo erros na lista
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
