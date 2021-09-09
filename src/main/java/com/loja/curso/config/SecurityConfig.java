9016194f0c52b54b320808d1186d6535c2388cd3
Kesia Amanda <kesia.ladeia@tgid.com.br>
2021-09-08 21:35:17 -0300

    Implementa autorização

--- src/main/java/com/loja/curso/config/SecurityConfig.java
+++ src/main/java/com/loja/curso/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.loja.curso.security.JWTAuthenticationFilter;
+import com.loja.curso.security.JWTAuthorizationFilter;
 import com.loja.curso.security.JWTUtil;
 
 @Configuration
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
