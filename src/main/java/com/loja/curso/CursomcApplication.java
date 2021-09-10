e311f92f4c7fbe4079a052c99b625acc7f9ef2bf
Kesia Amanda <kesia.ladeia@tgid.com.br>
2021-09-09 21:30:08 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/loja/curso/CursomcApplication.java
+++ src/main/java/com/loja/curso/CursomcApplication.java
@@ -1,12 +1,17 @@
 package com.loja.curso;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.loja.curso.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -14,6 +19,6 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\Users\\Miz\\Downloads\\peepoNotes.png");
 	}
 }
